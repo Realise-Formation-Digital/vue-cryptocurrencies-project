b26ce6814e24a8a6b9818191e86e5a5937829396
AMW <wolfer.amhc@gmail.com>
2021-02-04 02:32:28 +0100

    mod router.js

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -3,17 +3,13 @@ import VueRouter from 'vue-router'
 import Home from "@/components/Home";
 import BitcoinApi from "@/components/BitcoinApi";
 import FormulaireContact from "@/components/FormulaireContact";
-// import Crypto from "@/components/Crypto";
 import GoogleMap from "@/components/GoogleMap";
 import Information from "@/components/Information";
 import Apidecoinpaprika from "@/components/Apidecoinpaprika";
-// import AboutUs from '@/components/AboutUs';
 import HomePageKilian from "@/components/HomePageKilian";
 import Bitcoinlive from "@/components/Bitcoinlive";
 import Cryptocurrency from "@/components/Cryptocurrency";
 import PresentationCcs from '@/components/PresentationCcs';
-
-
 
 Vue.use(VueRouter)
 
